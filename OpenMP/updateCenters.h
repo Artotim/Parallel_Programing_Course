aa27a55078c82f791f6df492e8c03145ff25a4bd
Arthur Pereira <55847858+Artotim@users.noreply.github.com>
2021-11-28 02:55:59 -0300

    Delete OpenMP directory

--- OpenMP/updateCenters.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef FINAL_PARALLEL_UPDATECENTERS_H
-#define FINAL_PARALLEL_UPDATECENTERS_H
-#include "structs.h"
-
-kMeansOutput updateCenters(const float *matrix, mtxDimensions jobInfo, float *columnsWeights,
-                           int *membership, int nonZeroWeights);
-
-#endif //FINAL_PARALLEL_UPDATECENTERS_H
